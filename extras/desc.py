ed6fb583802657366d1ca74d193586bcf412a5b7
KristijanG <kristijan.georg@gmail.com>
2024-05-19 21:08:59 +0200

    Added desc.py

---
+++ extras/desc.py
@@ -0,0 +1,3 @@
+def desc(n):
+    for i in range(n,0,-1):
+        print(i)
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from swapmonth import swapmonth
-
+from extras.desc import desc
 print(swapmonth(1))
 print(swapmonth(5))
 print(swapmonth(12))
 print(swapmonth(13))
+desc(5)
